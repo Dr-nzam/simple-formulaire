df97b4ed19dccd2f14a9822d6b003fa57a281bdb
Dr-nzam <nzambouarmel@gmail.com>
2023-04-24 10:20:47 +0100

    p7

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class enregistrement (models.Model):
+    nom = models.CharField(max_length=128,)
+    classe = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nom
--- core/views.py
+++ core/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def formulaire (request):
+    nom = request.POST['nom'],
+    classe = request.POST['classe'],
+    
+    
     
     return render (request, 'core/index.html')
